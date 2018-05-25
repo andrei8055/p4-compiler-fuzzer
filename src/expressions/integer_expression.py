95facf78bbedb5bcdf291eb543f1541b7fcc7757
Mastertrap21 <mada_mastertrap21@yahoo.it>
2018-05-25 18:34:50 +0200

    Refactor (folder structure per component) + cleanup old stuff

--- src/expressions/integer_expression.py
+++
@@ -1,9 +0,0 @@
-class integer_expression(object):
-	type = 'integer_expression'
-	value = 0
-
-	def __init__(self, value):
-		self.name = value
-
-	def generate_code(self):
-		return self.value
--- src/expressions/string_expression.py
+++
@@ -1,9 +0,0 @@
-class string_expression(object):
-	type = 'string_expression'
-	value = ''
-
-	def __init__(self, value):
-		self.value = value
-			
-	def generate_code(self):
-		return self.value
